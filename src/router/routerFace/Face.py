9586e9e3dce66650f92ff3c98a359d18fb585ea3
Wilcar <ortizcolpaswilcardaniel@gmail.com>
2024-11-01 21:53:07 -0500

    actualizacion logica face

--- src/router/routerFace/Face.py
+++ src/router/routerFace/Face.py
@@ -2,20 +2,28 @@ from fastapi import APIRouter, UploadFile, File
 from typing import List
 from src.controller.controllerFace.Face import controller_Face 
 from src.config.routeConfig.routerConfig import getBaseRouter 
+from pydantic import BaseModel
+
+class RegisterFaceRequest(BaseModel):
+    nameUser: str
+    imagenes: List[str]
 
 BASE_URL = getBaseRouter()
 router = APIRouter()
 
-def create_route_everything_face(controller_Face):
 
+def create_route_everything_face(controller_Face):
     @router.post(f"{BASE_URL}/registerFace", tags=['Autenticacion Facial'])
-    async def register_face(nombre: str, images: List[UploadFile] = File(...)):
+    async def register_face(request: RegisterFaceRequest):
         print("Recibiendo datos para registrar la cara.")
         try:
-            response = await controller_Face.register_face(nombre, images)
-            return response
+            nameUser = request.nameUser
+            imagenes = request.imagenes
+            response = await controller_Face.register_face(nameUser, imagenes)
+            return nameUser
         except Exception as e:
             return {"error": str(e)}
+        
         
     @router.post(f"{BASE_URL}/authFace", tags=['Autenticacion Facial'])
     async def auth_face():
