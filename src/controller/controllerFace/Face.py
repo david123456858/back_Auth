7b325c279595ad0ce4b9e568de339959bcfa42b2
Wilcar <ortizcolpaswilcardaniel@gmail.com>
2024-11-02 13:38:53 -0500

    auth y login listo

--- src/controller/controllerFace/Face.py
+++ src/controller/controllerFace/Face.py
@@ -10,26 +10,28 @@ class controller_Face:
         self.caseFace_register = caseFace_register
         self.caseFace_auth = caseFace_auth        
 
-    async def register_face(self, user:userFace):
+    async def register_face(self, user:userFace)-> dict:
+        nameUser = user.nameUser
+        imagenes = user.imagenes
+        
+        if not nameUser or not isinstance(imagenes, list) or len(imagenes) != 10:
+            raise HTTPException(status_code=400, detail="Faltan argumentos")
         try:
-            # Verificar que se reciban los argumentos necesarios
-            nameUser = user.nameUser
-            imagenes = user.imagenes
-            if not nameUser or not imagenes:
-                raise HTTPException(status_code=400, detail="Faltan argumentos")
             result = await self.caseFace_register.register_face(user)
             return result
         except Exception as error:
             raise HTTPException(status_code=500, detail=f"Internal server error: {str(error)}")
-         
-    async def auth_face(self, user:userFace):
+        
+    
+    async def auth_face(self, user: userFace) -> dict:
+        nameUser = user.nameUser
+        imagenes = user.imagenes
+        
+        if not nameUser or not isinstance(imagenes, list) or len(imagenes) != 1:
+            raise HTTPException(status_code=400, detail="Faltan argumentos")
         try:
-            # Verificar que se reciban los argumentos necesarios
-            nameUser = user.nameUser
-            imagenes = user.imagenes
-            if not nameUser or not imagenes:
-                raise HTTPException(status_code=400, detail="Faltan argumentos")
             result = await self.caseFace_auth.auth_face(user)
             return result
-        except Exception as error:
-            raise HTTPException(status_code=500, detail=f"Internal server error: {str(error)}")
+        except Exception as e:
+            print(f"Error durante la autenticación: {e}")
+            raise HTTPException(status_code=500, detail="Error interno del servidor")
