67cf7a98a8136058987bfe41b6698ed2314333e0
david123456858 <juandavidperaltafuentes@gmail.com>
2024-11-02 21:04:34 -0500

    dejo esto funcionando

--- src/CaseUse/user/userFindType.py
+++ src/CaseUse/user/userFindType.py
@@ -1,4 +1,5 @@
 from fastapi import HTTPException
+from fastapi.responses import JSONResponse
 
 from src.util.verifyType import verifyType
 class caseUseFindTypeAuth:
@@ -9,13 +10,17 @@ class caseUseFindTypeAuth:
     def getTypeAuth(self,user:str):
         try:
             result = self.repository.get_user_by_id(user) 
+            
+            if not result:
+                return JSONResponse(status_code=404, content={"detail": "Not found user"})
+            
             resultUser = verifyType(result)
-            
-            response = {"data": {"nameUser": resultUser.nameUser, "typeAuth": resultUser.typeAuth}}
-            
-            if resultUser.typeAuth == "Questions": 
-                response["data"]["questions"] = user.questions
-            
-            return resultUser 
+            res = {
+                "userInfo":resultUser.__dict__ 
+            }
+            if resultUser.typeAuth == "Questions":
+                res["questions"] = result.questions
+                
+            return JSONResponse(content=res)
         except Exception as error:
             raise HTTPException(status_code=500,detail=f"internal error server {str(error)}")
