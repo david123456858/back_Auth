d724e3acd44fc62d3fa16e7feb7ef8ce64117ee7
david123456858 <juandavidperaltafuentes@gmail.com>
2024-11-04 21:38:00 -0500

    repository and conection db changes

--- src/framework/db/db.py
+++ src/framework/db/db.py
@@ -14,10 +14,18 @@ DB_NAME = os.getenv("PGDATABASE")
 DB_PORT = os.getenv("PGPORT", 5432)  
 
             
-DATABASE_URL = f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+DATABASE_URL = f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=prefer"
             
              
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={
+        "keepalives": 1,
+        "keepalives_idle": 30,      # Enviar keepalive cada 30 segundos de inactividad
+        "keepalives_interval": 10,  # Intervalo entre keepalives después de la primera señal
+        "keepalives_count": 5,       # Número de intentos antes de cerrar la conexión
+    })
             
             
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
